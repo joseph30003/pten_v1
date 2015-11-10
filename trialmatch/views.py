4586e8cd610a7a969f5aa345fdb6a00bc68464f9
= <=>
2015-11-09 23:55:46 -0500

    working es quesry

--- trialmatch/views.py
+++ trialmatch/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 import datetime
 from django.shortcuts import render
+import json,requests
 
 def homepage(request):
     return HttpResponse("This is clinical trial matching website")
@@ -13,4 +14,18 @@ def current_datetime(request):
     return HttpResponse(html)
 	
 def trial_list(request):
-	return render(request,'trialmatch/trial_list.html')
+	data = {
+    "query": {
+        "query_string": { "query": "cancer" }
+			}
+	}
+	records = requests.post('http://127.0.0.1:9200/testindex/trials/_search', data=json.dumps(data))
+	records = records.json()["hits"]["hits"]
+	ls = []
+	for i in records:
+		rec = {}
+		rec['title'] = i['_source']['title']
+		rec['purpose'] = i['_source']['purpose']
+		ls.append(rec)
+	records = ls
+	return render(request,'trialmatch/trial_list.html',{'records':records})
